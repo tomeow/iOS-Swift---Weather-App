7b154a14e46e70850f8860276553358f17cbbd66
Tomomi <tomomi.tamura2486@gmail.com>
2016-06-22 18:10:12 -0700

    Initial Commit

---
+++ 17_Whats_The_Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  17_Whats_The_Weather
+//
+//  Created by Tomomi Tamura on 6/22/16.
+//  Copyright © 2016 Tomomi Tamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
